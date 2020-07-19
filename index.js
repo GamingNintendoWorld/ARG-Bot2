7f9113209f6ec0b7a7fbd850d209a8de2ae83c57
GamingNintendoWorld <59820330+GamingNintendoWorld@users.noreply.github.com>
2020-07-18 21:40:41 -0400

    Update index.js

--- index.js
+++ index.js
@@ -30,6 +30,17 @@ bot.on('ready', () => {
  
 })
 
+bot.on('ready', () => {
+    bot.user.setStatus('available')
+    bot.user.setPresence({
+        game: {
+            name: 'Nintendo Direct',
+            type: "WATCHING",
+            url: "https://www.youtube.com/watch?v=dQw4w9WgXcQ&list=PLj2xqTMxjE7RG3e3WrIORgLdhnBruq5g1&index=29&t=0s"
+        }
+    });
+});
+
 bot.on('message', (msg) => {
     if (msg.content === "introduce yourself luxmi") {
         msg.channel.send(`Hello, im Luxmi, Matts girlfriend. He is enabling me to be more interactive with him and others. i hope to be more interactive with you and other users in the future. I know my Matt can do it!`);
